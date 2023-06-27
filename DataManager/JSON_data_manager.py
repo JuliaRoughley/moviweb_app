6651a52fae53cbaedf15b3aad8a21c62b28752f6
JuliaRoughley <roughleyjulia@gmail.com>
2023-06-27 16:57:35 +0100

    Updates including further html, updating and editing movies, api info, and currently trying to get the delete functionality to work

--- DataManager/JSON_data_manager.py
+++ DataManager/JSON_data_manager.py
@@ -1,6 +1,6 @@
 import json
-
 from DataManager.data_manager_interface import DataManagerInterface
+
 
 
 class JSONDataManager(DataManagerInterface):
@@ -42,10 +42,70 @@ class JSONDataManager(DataManagerInterface):
         }
         users_data.append(new_user)
 
-        with open("movie_data/movies.json", "w") as file:
+        with open(self.filename, "w") as file:
             json.dump(users_data, file, indent=4)
 
-    def add_new_movie(self):
-        pass
+    def add_new_movie(self, user_id, title, director, year, rating):
+        # Load the JSON data from the file
+        with open(self.filename, "r") as file:
+            users_data = json.load(file)
+
+        # Find the user for whom you want to add the movie
+        user = next((user for user in users_data if user["id"] == user_id), None)
+        if user is None:
+            # Handle user not found error
+            return
+
+        # Access the "movies" list for that user
+        movies = user["movies"]
+
+        # Check if the movie already exists in the user's list of movies
+        if any(movie["name"] == title for movie in movies):
+            # Movie already exists, handle the case appropriately
+            print(f"The movie '{title}' already exists in your list of favorite movies.")
+            return
+
+        # Generate a new movie ID
+        new_movie_id = len(movies) + 1
+
+        # Create the new movie dictionary
+        new_movie = {
+            "id": new_movie_id,
+            "name": title,
+            "director": director,
+            "year": year,
+            "rating": rating
+        }
+
+        # Append the new movie dictionary to the "movies" list
+        movies.append(new_movie)
+
+        # Save the modified JSON data back to the file
+        with open(self.filename, "w") as file:
+            json.dump(users_data, file, indent=4)
+
+    def movie_exists(self, user_id, movie_title):
+        users_data = self.open_movie_JSON_data()
+        user = next((user for user in users_data if user["id"] == user_id), None)
+        if user:
+            movies = user.get("movies", [])
+            return any(movie.get("name") == movie_title for movie in movies)
+        return False
+
+    def delete_movie(self, user_id, movie_id):
+        users_data = self.open_movie_JSON_data()
+        user = next((user for user in users_data if user["id"] == user_id), None)
+        if user is None:
+            # Handle user not found error
+            return
+        movies = user["movies"]
+        for movie in movies:
+            if movie["id"] == movie_id:
+                movie_to_delete = movie
+
+        movies.remove(movie_to_delete)
+
+        with open(self.filename, "w") as file:
+            json.dump(users_data, file, indent=4)
 
 
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from DataManager.JSON_data_manager import JSONDataManager
-import json
+from api_data_handling.api_data_handling import get_movie_details_by_name
+from urllib.parse import unquote, quote
 
 app = Flask(__name__)
 data_manager = JSONDataManager("movie_data/movies.json")
@@ -44,27 +45,110 @@ def add_new_user():
     return render_template('add_user.html')
 
 
-@app.route('/users/<user_id>/add_movie', methods=["GET", "POST"])
-def add_new_movie():
+@app.route('/users/<int:user_id>/add_movie', methods=["GET", "POST"])
+def add_new_movie(user_id):
     if request.method == 'POST':
+        movie = request.form.get('movie')
+        if not movie:
+            error_message = "Movie name is required."
+            return render_template('add_movie.html', error=error_message)
+
+        movie_details = get_movie_details_by_name(movie)
+        if movie_details is None:
+            error_message = f"I'm sorry, {movie} doesn't exist in the database."
+            return render_template('add_movie.html', error=error_message)
+
+        title = movie
+        director = movie_details["director"]
+        year = movie_details["year"]
+        rating = movie_details["rating"]
+
+        if data_manager.movie_exists(user_id, title):
+            error_message = f"{title} is already in your list of favorite movies."
+            return render_template('add_movie.html', error=error_message)
+
+        data_manager.add_new_movie(user_id, title, director, year, rating)
+        return redirect(f'/users/{user_id}')
+
+    return render_template("add_movie.html", user_id=user_id)
 
 
-    return render_template("add_movie.html")
+@app.route('/users/<int:user_id>')
+def show_user_movies(user_id):
+    # Retrieve user movies and other details from the JSON data manager
+    user_movies = data_manager.get_user_movies(user_id)
+    user_name = data_manager.get_user_name(user_id)
+
+    # Retrieve the success message from the URL parameter
+    success_message = request.args.get('success')
+
+    # Unquote the success message to remove URL encoding
+    if success_message:
+        success_message = unquote(success_message)
+
+    return render_template("user_movies.html", movies=user_movies, user_name=user_name, success_message=success_message)
 
 
-@app.route('/users/<user_id>/update_movie/<movie_id>')
-def update_movie():
-    pass
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['POST'])
+def update_movie(user_id, movie_id):
+    title = request.form.get('title')
+    director = request.form.get('director')
+    year = request.form.get('year')
+    rating = request.form.get('rating')
+
+    # Validate that all fields are filled
+    if not (title and director and year and rating):
+        error_message = "All fields are required."
+        return render_template('edit_movie.html', user_id=user_id, movie={"id": movie_id, "title": title, "director": director, "year": year, "rating": rating}, error=error_message)
+
+    # Retrieve the user's movies from the JSON data manager
+    user_movies = data_manager.get_user_movies(user_id)
+
+    # Find the movie to be updated
+    movie = next((movie for movie in user_movies if movie["id"] == movie_id), None)
+
+    if movie:
+        # Update the movie's details
+        movie["title"] = title
+        movie["director"] = director
+        movie["year"] = year
+        movie["rating"] = rating
+
+        # Save the updated movie data to the JSON file
+        data_manager.save_movie_JSON_data(user_movies)
+
+        # Construct the success message as a URL parameter
+        success_message = f"Movie '{quote(title)}' has been updated successfully."
+
+        # Redirect to the user's movie list page with the success message
+        return redirect(f'/users/{user_id}?success={quote(success_message)}')
+    else:
+        return "Movie not found"  # Or redirect to an error page
 
 
-@app.route('/users/<user_id>/edit_movie/<movie_id>')
-def edit_movie():
-    pass
+@app.route('/users/<int:user_id>/edit_movie/<int:movie_id>', methods=['GET'])
+def edit_movie(user_id, movie_id):
+    user_movies = data_manager.get_user_movies(user_id)
+    movie = next((movie for movie in user_movies if movie["id"] == movie_id), None)
+
+    if movie:
+        return render_template('edit_movie.html', user_id=user_id, movie=movie)
+    else:
+        return "Movie not found"  # Or redirect to an error page
 
 
-@app.route('/users/<user_id>/delete_movie/<movie_id>')
-def delete_movie():
-    pass
+@app.route('/users/<int:user_id>/delete_movie/<int:movie_id>', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    # Get the user_id and movie_id from the form data
+    user_id = int(request.form.get('user_id'))
+    movie_id = int(request.form.get('movie_id'))
+
+    # Call the delete_movie function from the JSON data manager
+    data_manager.delete_movie(user_id, movie_id)
+
+    return redirect(f'/users/{user_id}')
+
+
 
 
 if __name__ == '__main__':
